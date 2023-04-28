5a5a30fb771f10faf1ae7254888bb0742fd8d4ed
qwerdvd <2450899274@qq.com>
2023-04-28 13:00:44 +0800

    🐛修复开拓阅历`api`错误

--- StarRailUID/sruid_utils/api/mys/api.py
+++ StarRailUID/sruid_utils/api/mys/api.py
@@ -7,7 +7,7 @@ STAR_RAIL_SIGN_LIST_URL = f'{OLD_URL}/event/luna/home'
 STAR_RAIL_SIGN_EXTRA_INFO_URL = f'{OLD_URL}/event/luna/extra_info'
 STAR_RAIL_SIGN_EXTRA_REWARD_URL = f'{OLD_URL}/event/luna/extra_reward'
 STAR_RAIL_SIGN_URL = f'{OLD_URL}/event/luna/sign'
-STAR_RAIL_MONTH_INFO_URL = f'{NEW_URL}/event/srledger/month_info'  # 开拓阅历接口
+STAR_RAIL_MONTH_INFO_URL = f'{OLD_URL}/event/srledger/month_info'  # 开拓阅历接口
 
 STAR_RAIL_NOTE_URL = f'{NEW_URL}/game_record/app/hkrpg/api/note'  # 实时便签接口
 STAR_RAIL_INDEX_URL = f'{NEW_URL}/game_record/app/hkrpg/api/index'  # 角色橱窗接口
