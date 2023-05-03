03a8b6b3bd8a6ff540d8eaba24006d82332e234d
MingxuanGame <MingxuanGame@outlook.com>
2023-05-03 09:05:42 +0800

    :zap: 缓存图片

--- StarRailUID/starrailuid_roleinfo/utils.py
+++ StarRailUID/starrailuid_roleinfo/utils.py
@@ -3,8 +3,12 @@ from typing import List, TypeVar, Generator
 
 from PIL import Image
 from aiohttp import ClientSession
+from gsuid_core.data_store import get_res_path
 
 T = TypeVar("T")
+
+ROLEINFO_PATH = get_res_path() / 'StarRailUID' / 'roleinfo'
+ROLEINFO_PATH.mkdir(parents=True, exist_ok=True)
 
 
 def wrap_list(lst: List[T], n: int) -> Generator[List[T], None, None]:
@@ -13,6 +17,14 @@ def wrap_list(lst: List[T], n: int) -> Generator[List[T], None, None]:
 
 
 async def get_icon(url: str) -> Image.Image:
-    async with ClientSession() as client:
-        async with client.get(url) as resp:
-            return Image.open(BytesIO(await resp.read())).convert("RGBA")
+    name = url.split('/')[-1]
+    path = ROLEINFO_PATH / name
+    if (path).exists():
+        content = path.read_bytes()
+    else:
+        async with ClientSession() as client:
+            async with client.get(url) as resp:
+                content = await resp.read()
+                with open(path, "wb") as f:
+                    f.write(content)
+    return Image.open(BytesIO(content)).convert("RGBA")
