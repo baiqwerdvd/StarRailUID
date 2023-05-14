d5de4b7efbd0663700a34ece6b2b8fabe4970804
qwerdvd <2450899274@qq.com>
2023-05-14 17:14:03 +0800

    ✨面板查询带上UA

--- StarRailUID/sruid_utils/api/lulu/requests.py
+++ StarRailUID/sruid_utils/api/lulu/requests.py
@@ -4,12 +4,15 @@ from typing import Dict
 
 from httpx import AsyncClient
 
+from ..utils import _HEADER
+
 # from .models import EnkaData
 
 
 async def get_char_card_info(uid: str) -> Dict:
     async with AsyncClient(
         base_url='http://api.mihomo.me',
+        headers=_HEADER,
         timeout=30,
     ) as client:
         req = await client.get(f'/sr_info/{uid}')
