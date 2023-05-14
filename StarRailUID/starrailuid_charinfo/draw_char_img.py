9cec17ca92b57b25c2140447bc1d2dccdfabced7
RBAmeto <liurbang@gmail.com>
2023-05-14 15:37:12 +0800

    角色面板查询不存在时报错

--- StarRailUID/starrailuid_charinfo/draw_char_img.py
+++ StarRailUID/starrailuid_charinfo/draw_char_img.py
@@ -76,6 +76,8 @@ async def draw_char_info_img(raw_mes: str, sr_uid: str, url: Optional[str]):
     char_name = ' '.join(re.findall('[\u4e00-\u9fa5]+', raw_mes))
 
     char_data = await get_char_data(sr_uid, char_name)
+    if isinstance(char_data,str):
+        return char_data
     char = await cal_char_info(char_data)
 
     # 放角色立绘
@@ -577,11 +579,11 @@ async def get_char_data(
 ) -> Union[Dict, str]:
     player_path = PLAYER_PATH / str(sr_uid)
     SELF_PATH = player_path / 'SELF'
-    char_id = await name_to_avatar_id(char_name)
+    # char_id = await name_to_avatar_id(char_name)
     if '开拓者' in char_name:
         char_name = '开拓者'
-    else:
-        char_name = await alias_to_char_name(char_id, char_name)
+    # else:
+    #     char_name = await alias_to_char_name(char_id, char_name)
 
     char_path = player_path / f'{char_name}.json'
     char_self_path = SELF_PATH / f'{char_name}.json'
