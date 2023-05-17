4b23038527ac98a8d3cbe3a99dc565aeec885365
qwerdvd <2450899274@qq.com>
2023-05-17 11:29:42 +0800

    ✨实现wiki资源下载

--- StarRailUID/utils/resource/download_from_cos.py
+++ StarRailUID/utils/resource/download_from_cos.py
@@ -29,51 +29,65 @@ async def download_all_file_from_cos():
     failed_list: List[Tuple[str, int, str]] = []
     TASKS = []
     async with ClientSession() as sess:
-        for resource_type in [
-            'character',
-            'character_portrait',
-            'consumable',
-            'element',
-            'light_cone',
-            'relic',
-            'skill',
-        ]:
-            file_dict = resource_map[resource_type]
-            logger.info(f'[cos]数据库[{resource_type}]中存在{len(file_dict)}个内容!')
-            temp_num = 0
-            for file_name, file_info in file_dict.items():
-                name = file_name
-                size = file_info['size']
-                url = file_info['url']
-                path = Path(RESOURCE_PATH / resource_type / name)
-                if path.exists():
-                    is_diff = size == str(os.stat(path).st_size)
-                else:
-                    is_diff = True
-                if (
-                    not path.exists()
-                    or not os.stat(path).st_size
-                    or not is_diff
-                ):
-                    logger.info(f'[cos]开始下载[{resource_type}]_[{name}]...')
-                    temp_num += 1
-                    TASKS.append(
-                        asyncio.wait_for(
-                            download_file(url, resource_type, name, sess),
-                            timeout=60,
+        for res_type in ['resource', 'wiki']:
+            if res_type == 'resource':
+                logger.info('[cos]开始下载资源文件...')
+                resource_type_list = [
+                    'character',
+                    'character_portrait',
+                    'consumable',
+                    'element',
+                    'light_cone',
+                    'relic',
+                    'skill',
+                ]
+            else:
+                logger.info('[cos]开始下载wiki文件...')
+                resource_type_list = [
+                    'lightcone',
+                    'material for role',
+                    'relic',
+                    'role',
+                ]
+            for resource_type in resource_type_list:
+                file_dict = resource_map[res_type][resource_type]
+                logger.info(
+                    f'[cos]数据库[{resource_type}]中存在{len(file_dict)}个内容!'
+                )
+                temp_num = 0
+                for file_name, file_info in file_dict.items():
+                    name = file_name
+                    size = file_info['size']
+                    url = file_info['url']
+                    path = Path(RESOURCE_PATH / resource_type / name)
+                    if path.exists():
+                        is_diff = size == str(os.stat(path).st_size)
+                    else:
+                        is_diff = True
+                    if (
+                        not path.exists()
+                        or not os.stat(path).st_size
+                        or not is_diff
+                    ):
+                        logger.info(f'[cos]开始下载[{resource_type}]_[{name}]...')
+                        temp_num += 1
+                        TASKS.append(
+                            asyncio.wait_for(
+                                download_file(url, resource_type, name, sess),
+                                timeout=60,
+                            )
                         )
-                    )
-                    # await download_file(url, FILE_TO_PATH[file], name)
-                    if len(TASKS) >= 10:
-                        await _download(TASKS)
-            else:
-                await _download(TASKS)
-            if temp_num == 0:
-                im = f'[cos]数据库[{resource_type}]无需下载!'
-            else:
-                im = f'[cos]数据库[{resource_type}]已下载{temp_num}个内容!'
-            temp_num = 0
-            logger.info(im)
+                        # await download_file(url, FILE_TO_PATH[file], name)
+                        if len(TASKS) >= 10:
+                            await _download(TASKS)
+                else:
+                    await _download(TASKS)
+                if temp_num == 0:
+                    im = f'[cos]数据库[{resource_type}]无需下载!'
+                else:
+                    im = f'[cos]数据库[{resource_type}]已下载{temp_num}个内容!'
+                temp_num = 0
+                logger.info(im)
     if failed_list:
         logger.info(f'[cos]开始重新下载失败的{len(failed_list)}个文件...')
         for url, file, name in failed_list:
