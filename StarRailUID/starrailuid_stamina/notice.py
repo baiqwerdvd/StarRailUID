2d442394df0798b5c3cefe82cb5fcf9ec8660302
mengluo04 <32565940+mengluo04@users.noreply.github.com>
2024-09-07 12:51:02 +0800

    新增体力推送 (#33)
    * GsStrConfi新增体力推送
    
    * 修改星铁推送管理类名
    
    * 修改定时体力推送的bug

--- StarRailUID/starrailuid_stamina/notice.py
+++ StarRailUID/starrailuid_stamina/notice.py
@@ -2,17 +2,18 @@ from typing import Dict
 
 from gsuid_core.gss import gss
 from gsuid_core.logger import logger
-from gsuid_core.utils.database.models import GsPush, GsUser
+from gsuid_core.utils.database.models import GsUser
 
 from ..sruid_utils.api.mys.models import DailyNoteData
 from ..starrailuid_config.sr_config import srconfig
+from ..utils.database.model import SrPush
 from ..utils.mys_api import mys_api
 
 MR_NOTICE = "\n可发送[srmr]或者[sr每日]来查看更多信息!\n"
 
 NOTICE = {
-    "stamina": f"你的开拓力快满啦!{MR_NOTICE}",
-    "go": f"你有派遣信息即将可收取!{MR_NOTICE}",
+    "stamina": f"你的开拓力已达提醒阈值!{MR_NOTICE}",
+    "go": f"你的派遣已全部完成!{MR_NOTICE}",
 }
 
 
@@ -26,7 +27,7 @@ async def get_notice_list() -> Dict[str, Dict[str, Dict]]:
                 if isinstance(raw_data, int):
                     logger.error(f"[sr推送提醒]获取{user.sr_uid}的数据失败!")
                     continue
-                push_data = await GsPush.select_data_by_uid(user.sr_uid, "sr")
+                push_data = await SrPush.select_data_by_uid(user.sr_uid, "sr")
                 msg_dict = await all_check(
                     user.bot_id,
                     raw_data,
@@ -47,63 +48,72 @@ async def all_check(
     uid: str,
 ) -> Dict[str, Dict[str, Dict]]:
     for mode in NOTICE.keys():
+        _check = await check(
+            mode,
+            raw_data,
+            push_data.get(f"{mode}_value", 0),
+        )
+
         # 检查条件
         if push_data[f"{mode}_is_push"] == "on":
-            if srconfig.get_config("CrazyNotice").data:
-                if not await check(mode, raw_data, push_data[f"{mode}_value"]):
-                    await GsPush.update_data_by_uid(
+            if not srconfig.get_config("CrazyNotice").data:
+                if not _check:
+                    await SrPush.update_data_by_uid(
                         uid, bot_id, "sr", **{f"{mode}_is_push": "off"}
                     )
-                continue
+            continue
+
         # 准备推送
-        if await check(mode, raw_data, push_data[f"{mode}_value"]):
+        if _check:
             if push_data[f"{mode}_push"] == "off":
                 pass
-            # on 推送到私聊
             else:
+                notice = NOTICE[mode]
+                if isinstance(_check, int):
+                    notice += f"（当前值: {_check}）"
+
                 # 初始化
                 if bot_id not in msg_dict:
                     msg_dict[bot_id] = {"direct": {}, "group": {}}
+                    direct_data = msg_dict[bot_id]["direct"]
+                    group_data = msg_dict[bot_id]["group"]
 
+                # on 推送到私聊
                 if push_data[f"{mode}_push"] == "on":
                     # 添加私聊信息
-                    if user_id not in msg_dict[bot_id]["direct"]:
-                        msg_dict[bot_id]["direct"][user_id] = NOTICE[mode]
+                    if user_id not in direct_data:
+                        direct_data[user_id] = notice
                     else:
-                        msg_dict[bot_id]["direct"][user_id] += NOTICE[mode]
-                    await GsPush.update_data_by_uid(
-                        uid, bot_id, "sr", **{f"{mode}_is_push": "on"}
-                    )
+                        direct_data[user_id] += notice
                 # 群号推送到群聊
                 else:
                     # 初始化
                     gid = push_data[f"{mode}_push"]
-                    if gid not in msg_dict[bot_id]["group"]:
-                        msg_dict[bot_id]["group"][gid] = {}
+                    if gid not in group_data:
+                        group_data[gid] = {}
 
-                    if user_id not in msg_dict[bot_id]["group"][gid]:
-                        msg_dict[bot_id]["group"][gid][user_id] = NOTICE[mode]
+                    if user_id not in group_data[gid]:
+                        group_data[gid][user_id] = notice
                     else:
-                        msg_dict[bot_id]["group"][gid][user_id] += NOTICE[mode]
-                    await GsPush.update_data_by_uid(
-                        uid, bot_id, "sr", **{f"{mode}_is_push": "on"}
-                    )
+                        group_data[gid][user_id] += notice
+
+                await SrPush.update_data_by_uid(
+                    uid, bot_id, 'sr', **{f"{mode}_is_push": "on"}
+                )
     return msg_dict
 
 
 async def check(mode: str, data: DailyNoteData, limit: int) -> bool:
-    if mode == "resin":
+    if mode == "stamina":
         if data.current_stamina >= limit:
             return True
         if data.current_stamina >= data.max_stamina:
             return True
         return False
     if mode == "go":
+        count = 0
         for i in data.expeditions:
             if i.status == "Ongoing":
-                if i.remaining_time <= limit * 60:
-                    return True
-            else:
-                return True
-        return False
+                count += 1
+        return count == 0
     return False
