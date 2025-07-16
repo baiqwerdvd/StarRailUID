17fb653b34a119b69b8e0515e1483fc4b2c5a1d9
ilnli <220916405+ilnli@users.noreply.github.com>
2025-07-17 00:06:36 +0800

    🐛 Support eng name match

--- StarRailUID/starrailuid_wiki/__init__.py
+++ StarRailUID/starrailuid_wiki/__init__.py
@@ -28,7 +28,7 @@ sv_sr_guide = SV("星铁攻略")
 
 @sv_sr_wiki.on_prefix("角色图鉴")
 async def send_role_wiki_pic(bot: Bot, ev: Event):
-    char_name = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    char_name = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{char_name}图鉴")
     if "开拓者" in str(char_name):
         char_name = "开拓者"
@@ -47,7 +47,7 @@ async def send_role_wiki_pic(bot: Bot, ev: Event):
 
 @sv_sr_guide.on_prefix("角色攻略")
 async def send_role_guide_pic(bot: Bot, ev: Event):
-    char_name = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    char_name = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{char_name}图鉴")
     char_id = await alias_to_char_id(char_name)
     if char_id is None:
@@ -68,7 +68,7 @@ async def send_role_guide_pic(bot: Bot, ev: Event):
 
 @sv_sr_guide.on_prefix("光锥攻略")
 async def send_weapon_guide_pic(bot: Bot, ev: Event):
-    msg = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    msg = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{msg}图鉴")
     light_cone_id = await name_to_weapon_id(msg)
     img = GUIDE_LIGHT_CONE_PATH / f"{light_cone_id}.png"
@@ -82,7 +82,7 @@ async def send_weapon_guide_pic(bot: Bot, ev: Event):
 
 @sv_sr_wiki.on_prefix("遗器")
 async def send_relic_wiki_pic(bot: Bot, ev: Event):
-    msg = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    msg = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{msg}遗器")
     set_id = await name_to_relic_set_id(msg)
     img = WIKI_RELIC_PATH / f"{set_id}.png"
@@ -96,7 +96,7 @@ async def send_relic_wiki_pic(bot: Bot, ev: Event):
 
 @sv_sr_wiki.on_prefix("突破材料")
 async def send_material_for_role_wiki_pic(bot: Bot, ev: Event):
-    char_name = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    char_name = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{char_name}突破材料")
     if "开拓者" in str(char_name):
         char_name = "开拓者"
@@ -115,7 +115,7 @@ async def send_material_for_role_wiki_pic(bot: Bot, ev: Event):
 
 @sv_sr_wiki.on_prefix("武器")
 async def send_light_cone_wiki_pic(bot: Bot, ev: Event):
-    msg = " ".join(re.findall("[\u4e00-\u9fa5]+", ev.text))
+    msg = " ".join(re.findall("[a-zA-Z_\u4e00-\u9fa5]+", ev.text))
     logger.info(f"开始获取{msg}武器")
     light_cone_id = await name_to_weapon_id(msg)
     img = WIKI_LIGHT_CONE_PATH / f"{light_cone_id}.png"
